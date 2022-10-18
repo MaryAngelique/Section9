e4b9cc7db8b3b236bc5ef5415de3e4492a188f9b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-18 16:54:33 -0400

    Implemented ITelephone class

---
+++ src/com/timbuchalka/Interface/ITelephone.java
@@ -0,0 +1,11 @@
+package com.timbuchalka.Interface;
+
+public class ITelephone {
+
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+
+}
