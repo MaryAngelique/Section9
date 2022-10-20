d6c2c51b5f8dff35538430cfa244271de09e64b9
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-20 14:51:05 -0400

    Implemented Override method for List method write

--- src/com/timbuchalka/Interface/Challenge/Player.java
+++ src/com/timbuchalka/Interface/Challenge/Player.java
@@ -43,9 +43,19 @@ public class Player implements ISavable {
     public void setStrength(int strength) {
         this.strength = strength;
     }
-
     public void setWeapon(String weapon) {
         this.weapon = weapon;
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values = new ArrayList<String>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        values.add(3, this.weapon);
+
+        return values;
     }
 
 
