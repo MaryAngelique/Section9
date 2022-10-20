af80364537fdc14f7a28d54338c526883e3645f6
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-20 12:27:25 -0400

    Implemented getter in Player class

--- src/com/timbuchalka/Interface/Challenge/Player.java
+++ src/com/timbuchalka/Interface/Challenge/Player.java
@@ -1,4 +1,34 @@
 package com.timbuchalka.Interface.Challenge;
 
-public class Player {
+import java.util.List;
+
+public class Player implements ISavable {
+
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength, String weapon) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = weapon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
 }
