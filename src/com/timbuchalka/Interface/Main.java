0869b5af687d92133fcd0e46fd3689b813ec414b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-20 10:50:36 -0400

    Implemented Telephone in Main class method

--- src/com/timbuchalka/Interface/Main.java
+++ src/com/timbuchalka/Interface/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Telephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
 
+        timsPhone = new MobilePhone(24565);
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
 
     }
 
