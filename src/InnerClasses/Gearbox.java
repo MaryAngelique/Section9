252b98605867d5063b8495dfe2712cac9ca727a1
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-21 10:23:05 -0400

    Implemented Gearbox class

---
+++ src/InnerClasses/Gearbox.java
@@ -0,0 +1,12 @@
+package InnerClasses;
+
+public class Gearbox {
+
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int currentGear = 0;
+    private boolean clutchIsIn;
+
+
+
+}
