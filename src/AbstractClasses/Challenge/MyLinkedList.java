565094d0799d6276472dd7ad733edfe388c877d3
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-26 10:08:43 -0400

    Implemented traverse override method with an if else statement to print out if the list is empty else go to the next value

--- src/AbstractClasses/Challenge/MyLinkedList.java
+++ src/AbstractClasses/Challenge/MyLinkedList.java
@@ -94,7 +94,14 @@ public class MyLinkedList {
 
     @Override
     public void traverse(ListItem root) {
-
+        if(root == null) {
+            System.out.println("The list is empty");
+        } else {
+            while (root != null) {
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
     }
 
 }
